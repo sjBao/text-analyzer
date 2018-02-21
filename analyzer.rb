df4e99d8ab5b146f82e4c6c36dc917b0f8a18080
typeoftony <tony@vody.com>
2018-02-21 13:57:13 -0500

    Add more statics and stopwords constant

--- analyzer.rb
+++ analyzer.rb
@@ -1,11 +1,21 @@
-lines = File.readlines("oliver.txt")
+lines = File.readlines('oliver.txt')
 text = lines.join
 
+STOPWORDS = %w[the a by on for of are with just but and to the my I has some in].freeze
+
 line_count = lines.size
-puts "#{line_count} lines"
-
 total_characters = text.length
-puts "#{total_characters} characters"
-
 total_characters_nospace = text.gsub(/\s+/, '').length
+# p text.scan(/[\w\-]+/).length
+word_count = text.split.length
+sentence_count = text.split(/\.|\?|!/).length
+paragraph_count = text.split(/\n\n/).length
+
+puts "#{line_count} lines"
+puts "#{total_characters} characters"
 puts "#{total_characters_nospace} characters excluding spaces"
+puts "#{word_count} words"
+puts "#{sentence_count} sentences"
+puts "#{paragraph_count} paragraphs"
+puts "#{word_count / sentence_count} words per sentences (average)"
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
