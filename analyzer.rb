f4c8050489ecfd26dad1a5eca5c76a3839a004b4
typeoftony <tony@vody.com>
2018-02-21 13:36:55 -0500

    Analyze characters excluding spaces

--- analyzer.rb
+++ analyzer.rb
@@ -1,7 +1,11 @@
 lines = File.readlines("oliver.txt")
-line_count = lines.size
 text = lines.join
 
+line_count = lines.size
 puts "#{line_count} lines"
+
 total_characters = text.length
 puts "#{total_characters} characters"
+
+total_characters_nospace = text.gsub(/\s+/, '').length
+puts "#{total_characters_nospace} characters excluding spaces"
